82ce60e5a1a2af681c472070c6cb27a51afd124d
Obinna Ikeh <hikenike6@gmail.com>
2022-02-20 16:25:24 +0100

    initial brew formula setup

---
+++ larry.rb
@@ -0,0 +1,17 @@
+class Larry < Formula
+  desc "Larry 🐦 is a really simple Twitter bot generator that tweets random repositories from Github built in Go"
+  homepage "https://github.com/ezeoleaf/larry"
+  url "https://github.com/ezeoleaf/larry/archive/refs/tags/0.4.0.tar.gz"
+  sha256 "5bd34b2f1f3e124fd66a62b55e3b97d7af3c830f0c49de785b05cec50d111585"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+      system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    assert_match /Twitter bot that publishes random information from providers/, shell_output("#{bin}/larry -h", 1)
+  end
+end
